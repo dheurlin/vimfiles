cbf64d3b09a8711210b6660dce11a0054f602d9f
dheurlin <daniel.heurlin@gmail.com>
2017-08-31 01:33:44 +0200

    Change zathura to mupdf, auto-refresh not working yet

--- md-make.sh
+++ md-make.sh
@@ -3,7 +3,7 @@
 TARGET="$1"
 
 # Build the pdf
-pandoc -o "$TARGET.pdf" --template="/Users/$USER/Dropbox/pandoc_templates/notes.latex" "$TARGET"
+pandoc -o "$TARGET.pdf" "$TARGET"
 
-# Make zathura refresh
-killall -HUP zathura &> /dev/null
+# Make mupdf refresh
+killall -s SIGHUP mupdf-gl &> /dev/null
