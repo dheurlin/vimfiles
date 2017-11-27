2e205b92b27d6a7d7afa7a64b9f53961a9ee216a
Daniel Heurlin <heurlin@student.chalmers.se>
2017-11-27 12:15:20 +0100

    sdjflksjdklj

--- .vimrc
+++ .vimrc
@@ -407,3 +407,5 @@ let g:syntastic_auto_loc_list = 1
 let g:syntastic_check_on_open = 1
 let g:syntastic_check_on_wq = 0
 let g:syntastic_java_javac_config_file_enabled = 1
+
+let g:syntastic_mode_map = { 'mode': 'passive' }
--- md-make.sh
+++ md-make.sh
@@ -3,7 +3,8 @@
 TARGET="$1"
 
 # Build the pdf
-pandoc -o "$TARGET.pdf" --template="/Users/$USER/Dropbox/pandoc_templates/notes.latex" "$TARGET"
+# pandoc -o "$TARGET.pdf" --template="/Users/$USER/Dropbox/pandoc_templates/notes.latex" "$TARGET"
+pandoc -o "$TARGET.pdf" "$TARGET"
 
 # Make zathura refresh
 killall -HUP zathura &> /dev/null
