d6813fc90535566ded3f050cf641e949a53e9aaa
dheurlin <daniel.heurlin@gmail.com>
2017-12-22 17:11:28 +0100

    Markdown references or whatever

--- md-make.sh
+++ md-make.sh
@@ -4,7 +4,7 @@ TARGET="$1"
 
 # Build the pdf
 # pandoc -o "$TARGET.pdf" --template="/Users/$USER/Dropbox/pandoc_templates/notes.latex" "$TARGET"
-pandoc -o "$TARGET.pdf" "$TARGET"
+pandoc -o "$TARGET.pdf" "$TARGET" --standalone -smart -F pandoc-citeproc --csl ~/.vim/ieee.csl
 
 # Make zathura refresh
 killall -HUP zathura &> /dev/null
