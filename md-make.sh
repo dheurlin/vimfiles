6bfa9619785770f910b5e15ca10a78b167bd8096
Daniel Heurlin <heurlin@student.chalmers.se>
2018-12-04 18:07:41 +0100

    Chagne to xelatex in markdown make

--- md-make.sh
+++ md-make.sh
@@ -3,7 +3,8 @@
 TARGET="$1"
 
 # Build the pdf
-pandoc -o "$TARGET.pdf" "$TARGET" --standalone -F pandoc-citeproc --csl ~/.vim/ieee.csl
+# pandoc -o "$TARGET.pdf" "$TARGET" --standalone -F pandoc-citeproc --csl ~/.vim/ieee.csl
+pandoc -o "$TARGET.pdf" --pdf-engine=xelatex "$TARGET"
 
 # Make zathura refresh
 killall -HUP zathura &> /dev/null
